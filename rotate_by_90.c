3ed4eebcdb99d85dcad32bb51de8732aaf6d193d
Radhika10128 <52877654+Radhika10128@users.noreply.github.com>
2019-07-20 00:39:52 +0530

    Add files via upload
    Rotate the matrix by 90 degree

---
+++ rotate_by_90.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int x,y;
+printf("Enter the size");
+scanf("%d %d",&x,&y);
+int arr[x][y];
+for(int i=0;i<x;i++)
+for(int j=0;j<y;j++)
+scanf("%d",&arr[i][j]);
+
+for(int i=0;i<x/2;i++)
+{
+for(int j=0;j<y-i-1;j++)
+{
+int temp=arr[i][j];
+arr[i][j]=arr[x-1-j][i];
+arr[x-1-j][i]=arr[x-1-i][y-1-j];
+arr[x-1-i][y-1-j]=arr[j][y-1-i];
+arr[j][y-1-i]=temp;
+}
+}
+for(int i=0;i<x;i++)
+{
+for(int j=0;j<y;j++)
+{
+printf("%d ",arr[i][j]);
+}
+printf("\n");
+}
+}
