46b85ce304814f31a0582027af5cfe6de4720fc7
Radhika10128 <52877654+Radhika10128@users.noreply.github.com>
2019-07-20 00:46:14 +0530

    Add files via upload
    Given no of rooms,arrival date and departure date in a hotel .....predict whether structure is possible or not

---
+++ hotel_bookings.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+int n;
+int x=1,flag=0;
+cout<<"Enter the no of elements of first array";
+cin>>n;
+int n1;
+cout<<"Enter the no of elements of second array";
+cin>>n1;
+int arr1[n],arr2[n1];
+cout<<"Enter the elements of first array";
+for(int i=0;i<n;i++)
+{
+    cin>>arr1[i];
+}
+cout<<"Enter the elements of second array";
+for(int i=0;i<n;i++)
+{
+    cin>>arr2[i];
+}
+int k;
+cout<<"Enter the no of rooms";
+scanf("%d",&k);
+sort(arr2,arr2+n1);
+sort(arr1,arr1+n);
+for(int i=0;i<n-1;i++)
+{
+    if(arr2[i]>arr1[i+1])
+        x++;
+        if(k<x)
+        flag=1;
+}
+if(flag==1)
+    cout<<"False";
+else
+    cout<<"True";
+
+
+}
